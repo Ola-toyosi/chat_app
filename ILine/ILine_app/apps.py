9f6234eb93ee3d2087f2d5225576138569ff4ad1
Toyosi <toyosiola@yahoo.com>
2022-01-21 21:53:35 +0100

    1st commit

---
+++ ILine/ILine_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class IlineAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'ILine_app'
---
+++ ILine/ILine_app/views.py
@@ -0,0 +1,15 @@
+from email import message
+from django.shortcuts import render
+from .models import Message
+
+
+# Create your views here.
+
+def index(request):
+    return render(request, 'ILine_app/index.html')
+
+def room(request, room_name):
+    username = request.GET.get('username', 'Anonymous')
+    messages = Message.objects.filter(room=room_name)[0:25]
+
+    return render(request, 'ILine_app/room.html', {'room_name': room_name, 'username': username, 'messages': messages })
